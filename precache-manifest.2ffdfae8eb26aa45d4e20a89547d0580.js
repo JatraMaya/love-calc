3b570c52977d0fd4b54e09afaca2e1f1bcc1b097
JatraMaya <ahmad.f.iksan@gmail.com>
2020-09-10 08:03:44 +0800

    Updates

---
+++ precache-manifest.2ffdfae8eb26aa45d4e20a89547d0580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab6918c18406a2e32a204c9c1e92051",
+    "url": "/love-calc/index.html"
+  },
+  {
+    "revision": "f3f2c32648a53497e0fa",
+    "url": "/love-calc/static/css/main.32133118.chunk.css"
+  },
+  {
+    "revision": "ad1445c43480d612684a",
+    "url": "/love-calc/static/js/2.864d05b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/love-calc/static/js/2.864d05b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f2c32648a53497e0fa",
+    "url": "/love-calc/static/js/main.5bf54a67.chunk.js"
+  },
+  {
+    "revision": "ddbba98e732c8eab5ed2",
+    "url": "/love-calc/static/js/runtime-main.2f9c5a66.js"
+  }
+]);
